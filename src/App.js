e0ac99639cd297fcf7e240a7abdd04ba826d40e3
Ulzii <uxb6317@rit.edu>
2019-08-11 22:13:02 -0400

    shop page + collection preview

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./collection-item.style.scss";
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className='image'
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
